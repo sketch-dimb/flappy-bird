28ed525dca1ef9329a5d54bc8963a842cf24220e
sketch-dimb <84538455+sketch-dimb@users.noreply.github.com>
2022-01-21 14:38:37 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # . . . .
+    . . # . .
+    . . # . .
+    `)
+let timer = 10
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # . . . .
+    . # . . .
+    . # . . .
+    `)
+timer = 10
+basic.showLeds(`
+    . # . . #
+    . # . . #
+    . . # . #
+    . # . . .
+    . # . . #
+    `)
+timer = 10
+basic.showLeds(`
+    . # . . #
+    . # . . #
+    . . . . #
+    . # . # .
+    . # . . #
+    `)
+timer = 10
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
